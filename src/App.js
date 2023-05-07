785002f5257444d188f55dd4394c80cadb8fef43
Thiago <101950809+Azbito@users.noreply.github.com>
2023-05-06 23:21:52 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useRef } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const count = useRef(0)
+
+  useEffect(() => {
+    count.current = count.current + 1
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <b>{count.current}</b>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
